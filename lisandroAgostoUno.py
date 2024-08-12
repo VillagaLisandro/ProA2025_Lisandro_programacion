3abc4b3f37c97904865644040ec9544eb0ccf087
VillagraLisandro <162600643+VillagaLisandro@users.noreply.github.com>
2024-08-12 12:15:22 -0300

    Add files via upload

---
+++ lisandroAgostoUno.py
@@ -0,0 +1,31 @@
+def menu():
+  menuInicio = '''
+  #########################################      
+  #     1 - añadir tarea                  #
+  #     2 - ver todas las tareas          #
+  #     3 - marcar tarea como completa    #
+  #     4 - Salir                         #
+  ######################################### '''
+  print(menuInicio)
+  opcion = int(input("Ingrese la opción deseada: "))
+  return opcion
+
+while True:
+  opcion = menu()
+  if opcion == 1:
+      Tareas_Añadidas = []
+      for _ in range(1):
+          añadeUnaTarea = str(input("ingrese su tarea"))
+          Tareas_Añadidas.append
+  elif opcion == 2:
+      print("las tareas ingresadas son:", Tareas_Añadidas)
+ 
+ 
+ 
+ 
+      
+      
+      
+      
+
+    
