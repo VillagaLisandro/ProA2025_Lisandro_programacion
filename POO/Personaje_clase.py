7194793ec88724463d23194aaf08451a75696196
VillagraLisandro <162600643+VillagaLisandro@users.noreply.github.com>
2024-09-09 12:23:15 -0300

    Add files via upload

--- POO/Personaje_clase.py
+++ POO/Personaje_clase.py
@@ -1,11 +1,44 @@
 class Personaje:
     #atributo de clase
     estado = True #vivo
+    vida = 100 
     
     #constructor / atributo de instancia
-    def __init__(self, nombre, altura, velocidad, fuerza):
+    def __init__(self, nombre, altura, velocidad, resistencia, fuerza):
         self.nombre  = nombre 
         self.altura = altura
         self.velocidad = velocidad
+        self.resistencia = resistencia 
         self.fuerza = fuerza 
+        self.estado = Personaje.estado
+        self.vida = Personaje.vida
+        
+    
+    def atacar(self, otro_personaje):
+        danio = self.fuerza = (otro_personaje.resistencia)
+        #danio = max (0, danio)
+        print(f"({self.nombre} ataca a {otro_personaje.nombre}causado " )
+        otro_personaje.recibir_dano(danio)
+       # else:
+        #    print(f"")
+
+        
+        
+    def recibir_dano(self, cantidad):
+        if self.estado:
+            self.vida = self.vida - cantidad
+            print(f"{self.nombre} resibe {cantidad} de danio. Vida restante     ")
+            if self.via <= 0:
+             self.vida = 0 
+            print(f"{self.nombre}ha muerto")
+        else:
+            print(f"{self.nombre}ya esta muerto ")
+            
+            
+                
+    
+    def mostrar_info(sef): 
+        
+        print()
+        
         
--- POO/TP2_ POO/carrera.py
+++ POO/TP2_ POO/carrera.py
@@ -1,42 +1,36 @@
-from Personaje_clase import Personaje
-
-
 personaje = []
 def menu():
   menuInicio = '''
   #########################################      
   #     1 - crear personaje               #
   #     2 - mostrar personaje             #
-  #     3 - salir                         # 
+  #     3 - jugar                         # 
+  #     4 - salir                         # 
   ######################################### '''
   print(menuInicio)
   opcion = int(input("ingrese su opcion elegida"))
   from typing import List
-  from Personaje_clase import Personaje 
-  
+  from Personaje_clase import Personaje
   p1 = Personaje("superman", 189, 90, 105)
   print(f"el personajese llama {p1.nombre}") 
   return opcion 
 cantidadPersonaje = 0 
-nuevo_personaje = Personaje(nombre, altura, velocidad, resistencia, fuerza)
-personaje.append(nuevo_personaje)
 while True:
      opcion =  menu()          
      if opcion == 1:
          cantidadPersonaje = cantidadPersonaje + 1
-         nombre = str(input("ingrese el nombre de su personaje:)")
-         altura = int(input(""))
-         velocidad = int(input("ingrese la velocidad del personaje"))
-         resistencia = int(input(""))
-         fuerza = int(input(""))
-        
+         personaje.append(input("nombre:"))
+         personaje.append(input("altura:"))
+         personaje.append(input("velocidad:"))
+         personaje.append(input("resistencia:"))
+         personaje.append(input("fuerza:"))
      elif opcion == 2:
-         cantidadPersonaje += 1
-         print(f"El personaje {nuevo_personaje.nombre} fue creado  con exito")
-         print(f"cantidad de personajes creados:{cantidadPersonaje}")
          print(personaje)  
-     elif opcion == 3:  
+     elif opcion == 3:
+         print("")
+     elif opcion == 4:  
          print("gracias por utilizar este programa")
+         break
          
 
          
